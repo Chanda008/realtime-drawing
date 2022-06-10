d2f840c6ce6f00e1ec4532bde8e7c058c2fedcec
Chanda008 <83400660+Chanda008@users.noreply.github.com>
2022-06-10 16:25:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup(){
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
@@ -9,6 +15,10 @@ function setup(){
 
 function draw(){
     background("#969A97");
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+    document.getElementById("square_size").innerHTML = "Width and Height of the Square is " + difference;
 }
 
 function preload(){
@@ -22,5 +32,13 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " and noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+    
     }
 }
